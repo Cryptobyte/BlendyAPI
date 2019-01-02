96da8d7f5d7091f03320f99d32c743c53dcfd86d
Evan Merz <bananamerz@yahoo.com>
2019-01-01 19:13:32 -0800

    Added user model with username, password, email, date joined, rank, friends, and coins

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
-// todo: everything ... lol
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const user = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    joined: {type: Date, default: Date.now},
+    rank: Number,
+    friends: [ObjectId],
+    requests: [ObjectId],
+    coins: Number
+});
+
+module.exports = mongoose.model("User", user);
